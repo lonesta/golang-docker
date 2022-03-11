837d832fc5f37bb842ae8a18153a1cae760e02b9
lonesta <mini223@yandex.ru>
2022-03-11 18:41:59 +0400

    refactoring for use container struct

---
+++ docker/container.go
@@ -0,0 +1,147 @@
+package docker
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+	"github.com/google/uuid"
+)
+
+type Container struct {
+	ctx           context.Context
+	imageName     string
+	bashCommand   string
+	containerName string
+	containerID   string
+	cli           *client.Client
+}
+
+func NewContainer(ctx context.Context, imageName string, bashCommand string, client *client.Client) *Container {
+	return &Container{ctx: ctx, imageName: imageName, bashCommand: bashCommand, cli: client, containerName: uuid.NewString()}
+}
+
+//Create container from image with bash command and the random uuid name
+func (c *Container) Create() error {
+	searchResult, err := c.cli.ImageSearch(c.ctx, c.imageName, types.ImageSearchOptions{
+		Limit: 1,
+	})
+	if err != nil {
+		return err
+	}
+
+	if len(searchResult) == 0 {
+		return fmt.Errorf("image with name: %s not found", c.imageName)
+	}
+
+	reader, err := c.cli.ImagePull(c.ctx, c.imageName, types.ImagePullOptions{})
+	if err != nil {
+		return err
+	}
+
+	defer reader.Close()
+	_, err = io.Copy(os.Stdout, reader)
+	if err != nil {
+		return err
+	}
+
+	config := &types.ContainerCreateConfig{
+		Name: c.containerName,
+		Config: &container.Config{
+			Image:      c.imageName,
+			Cmd:        []string{c.bashCommand},
+			Entrypoint: []string{"/bin/bash", "-c"},
+			Tty:        true,
+		},
+	}
+
+	created, err := c.cli.ContainerCreate(c.ctx, config.Config, config.HostConfig, config.NetworkingConfig, config.Platform, config.Name)
+	if err != nil {
+		_ = c.cli.ContainerRemove(context.Background(), created.ID, types.ContainerRemoveOptions{Force: true})
+		return err
+	}
+
+	c.containerID = created.ID
+
+	return nil
+}
+
+// Start container
+// Context cancellation stop and remove container
+func (c *Container) Start() error {
+	err := c.cli.ContainerStart(c.ctx, c.containerID, types.ContainerStartOptions{})
+	if err != nil {
+		_ = c.cli.ContainerRemove(context.Background(), c.containerID, types.ContainerRemoveOptions{Force: true})
+		return err
+	}
+
+	return nil
+}
+
+//Events running a logs listener from a container
+func (c *Container) Events() (<-chan string, <-chan struct{}) {
+	logs := make(chan string)
+	done := make(chan struct{}, 1)
+	waitConditionNotRunning, errChan := c.cli.ContainerWait(context.Background(), c.containerID, container.WaitConditionNotRunning)
+
+	go func() {
+		out, err := c.cli.ContainerLogs(c.ctx, c.containerID, types.ContainerLogsOptions{ShowStdout: true, ShowStderr: true, Follow: true})
+		if err != nil {
+			logs <- err.Error()
+		}
+		defer out.Close()
+
+		go func() {
+			scan := bufio.NewScanner(out)
+			for scan.Scan() {
+				s := scan.Text()
+				logs <- s
+			}
+		}()
+
+		for {
+			select {
+			case err = <-errChan:
+				logs <- err.Error()
+				done <- struct{}{}
+				return
+			case status := <-waitConditionNotRunning:
+				_ = c.cli.ContainerRemove(context.Background(), c.containerID, types.ContainerRemoveOptions{Force: true})
+				switch status.StatusCode {
+				case 137:
+					err = fmt.Errorf("container id: %s stopped manually", c.containerID)
+				case 127:
+					err = fmt.Errorf("cannot start: %s", c.containerID)
+				default:
+					err = fmt.Errorf("container id: %s stopped with %d exit code", c.containerID, status.StatusCode)
+				}
+
+				logs <- err.Error()
+				done <- struct{}{}
+				return
+			case <-c.ctx.Done():
+				err = c.cli.ContainerStop(context.Background(), c.containerID, nil)
+				if err != nil {
+					logs <- err.Error()
+					done <- struct{}{}
+					return
+				}
+
+				err = c.cli.ContainerRemove(context.Background(), c.containerID, types.ContainerRemoveOptions{Force: true})
+				if err != nil {
+					logs <- err.Error()
+				}
+
+				done <- struct{}{}
+				return
+			}
+		}
+	}()
+
+	return logs, done
+}
--- main.go
+++ main.go
@@ -2,124 +2,67 @@ package main
 
 import (
 	"context"
-	"errors"
 	"flag"
 	"fmt"
-	"io"
+	"golang-docker/docker"
 	"os"
 	"os/signal"
-	"sync"
-	"syscall"
-	"time"
 
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
-	"github.com/google/uuid"
 )
 
 func main() {
-	image := flag.String("docker-image", "", "A name of a Docker image")
+	imageName := flag.String("docker-image", "", "A name of a Docker image")
 	bashCommand := flag.String("bash-command", "", "A bash command (to run inside this Docker image)")
+	cloudWatchGroupName := flag.String("cloudwatch-group", "", "A name of an AWS CloudWatch group")
+	cloudWatchStreamName := flag.String("cloudwatch-stram", "", "A name of an AWS CloudWatch stream")
+	cloudWatchRegion := flag.String("aws-region", "", "A name of an AWS region")
+	cloudWatchAccessKeyID := flag.String("aws-access-key-id", "", "Access key id AWS")
+	cloudWatchSecretAccessKey := flag.String("aws-secret-access-key", "", "Secret access key AWS")
 	flag.Parse()
 
-	if *image == "" {
-		panic("Cannot start without a docker image name")
+	//if *imageName == "" {
+	if *imageName == "" || *cloudWatchGroupName == "" || *cloudWatchStreamName == "" ||
+		*cloudWatchRegion == "" || *cloudWatchAccessKeyID == "" || *cloudWatchSecretAccessKey == "" {
+		fmt.Println("Cannot start without a docker image name")
+		return
+	}
+
+	cli, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
 
 	ctx, cancel := context.WithCancel(context.Background())
-
 	go func() {
 		exit := make(chan os.Signal, 1)
-		signal.Notify(exit, os.Interrupt, syscall.SIGTERM)
+		signal.Notify(exit, os.Interrupt, os.Kill)
 		<-exit
 		cancel()
 	}()
 
-	var wg sync.WaitGroup
-
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		err := Start(ctx, *image, []string{*bashCommand})
-		if err != nil {
-			fmt.Println(err)
-		}
-	}()
-
-	wg.Wait()
-	fmt.Println("Main done")
-}
-
-func Start(ctx context.Context, imageName string, bashCommand []string) (err error) {
-	var entrypoint = []string{"/bin/bash", "-c"}
-	cli, err := client.NewClientWithOpts(client.FromEnv)
+	ctn := docker.NewContainer(ctx, *imageName, *bashCommand, cli)
+	err = ctn.Create()
 	if err != nil {
-		return err
-	}
-
-	config := &types.ContainerCreateConfig{
-		Name: uuid.NewString(),
-		Config: &container.Config{
-			Image:      imageName,
-			Cmd:        bashCommand,
-			Entrypoint: entrypoint,
-			Tty:        true,
-		},
-	}
-
-	result, err := cli.ImageSearch(ctx, config.Config.Image, types.ImageSearchOptions{
-		Limit: 1,
-	})
-	if err != nil {
-		return err
-	}
-
-	if len(result) == 0 {
-		return errors.New(fmt.Sprintf("image with name: %s not found", config.Config.Image))
-	}
-
-	_, err = cli.ImagePull(ctx, config.Config.Image, types.ImagePullOptions{})
-	if err != nil {
-		return err
-	}
-
-	created, err := cli.ContainerCreate(ctx, config.Config, config.HostConfig, config.NetworkingConfig, config.Platform, config.Name)
-	if err != nil {
-		return err
-	}
-
-	err = cli.ContainerStart(ctx, created.ID, types.ContainerStartOptions{})
-	if err != nil {
-		return err
-	}
-
-	statusCh, errCh := cli.ContainerWait(ctx, created.ID, container.WaitConditionRemoved)
-
-	select {
-	case err = <-errCh:
-	case <-statusCh:
-		break
-	default:
-		out, err := cli.ContainerLogs(ctx, created.ID, types.ContainerLogsOptions{ShowStdout: true, ShowStderr: true, Follow: true})
-		if err != nil {
-			break
-		}
-
-		_, err = io.Copy(os.Stdout, out)
-		if err != nil {
-			break
-		}
-
-		time.Sleep(5 * time.Second)
-	}
-
-	err = cli.ContainerStop(context.Background(), created.ID, nil)
-	if err != nil {
+		fmt.Println(err)
 		return
 	}
 
-	err = cli.ContainerRemove(context.Background(), created.ID, types.ContainerRemoveOptions{Force: true})
+	err = ctn.Start()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-	return
+	logsChan, doneChan := ctn.Events()
+	for {
+		select {
+		case <-doneChan:
+			fmt.Println("FINISH")
+			return
+		case val := <-logsChan:
+			fmt.Println(val)
+		}
+	}
 }
